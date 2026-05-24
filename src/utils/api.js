f3a4638e1eecf3a81294bba8be66943fde4ad223
vivek dk <dkvivek8@gmail.com>
2026-05-24 19:54:07 +0530

    fix: CORS origin issues

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,7 +8,7 @@ import axios from "axios";
 const api = axios.create({
 
   baseURL:
-    import.meta.env.VITE_NODE_URL || 'http://localhost:5000',
+    import.meta.env.VITE_NODE_API_URL || 'http://localhost:5000',
 
   headers: {
     "Content-Type":
